a3e672e551df44c4f0fb87f70850e518fd97ffff
ksbisht <ksbisht941@gmail.com>
2022-02-27 13:25:20 +0530

    Implementing Service class method

---
+++ src/main/java/com/appdeveloperblog/app/ws/service/UserService.java
@@ -0,0 +1,9 @@
+package com.appdeveloperblog.app.ws.service;
+
+import com.appdeveloperblog.app.ws.shared.dto.UserDto;
+
+public interface UserService {
+
+	UserDto createUser(UserDto user);
+	
+}
---
+++ src/main/java/com/appdeveloperblog/app/ws/service/impl/UserServiceImpl.java
@@ -0,0 +1,15 @@
+package com.appdeveloperblog.app.ws.service.impl;
+
+import org.springframework.stereotype.Service;
+
+import com.appdeveloperblog.app.ws.service.UserService;
+import com.appdeveloperblog.app.ws.shared.dto.UserDto;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+	@Override
+	public UserDto createUser(UserDto user) {
+		return null;
+	}
+}

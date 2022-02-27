ff8380924a713f696c7a31936c2a2ddfe771873a
ksbisht <ksbisht941@gmail.com>
2022-02-27 17:54:43 +0530

    Autowire UserRepository into Service class

--- src/main/java/com/appdeveloperblog/app/ws/service/impl/UserServiceImpl.java
+++ src/main/java/com/appdeveloperblog/app/ws/service/impl/UserServiceImpl.java
@@ -21,6 +21,9 @@ public class UserServiceImpl implements UserService {
 		UserEntity userEntity = new UserEntity();
 		BeanUtils.copyProperties(user, userEntity);
 		
+		userEntity.setEncryptedPassword("test");
+		userEntity.setUserId("testUserId");
+		
 		UserEntity storedUserDetails = userRepository.save(userEntity);
 		
 		UserDto returnValue = new UserDto();

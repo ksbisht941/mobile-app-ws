83ca9bed4ac5e11d8895bd1c07605248f983065f
ksbisht <ksbisht941@gmail.com>
2022-02-27 17:47:34 +0530

    Autowire UserRepository into Service class

--- src/main/java/com/appdeveloperblog/app/ws/service/impl/UserServiceImpl.java
+++ src/main/java/com/appdeveloperblog/app/ws/service/impl/UserServiceImpl.java
@@ -1,15 +1,31 @@
 package com.appdeveloperblog.app.ws.service.impl;
 
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.appdeveloperblog.app.ws.UserRepository;
+import com.appdeveloperblog.app.ws.io.entity.UserEntity;
 import com.appdeveloperblog.app.ws.service.UserService;
 import com.appdeveloperblog.app.ws.shared.dto.UserDto;
 
 @Service
 public class UserServiceImpl implements UserService {
 
+	@Autowired
+	UserRepository userRepository;
+	
 	@Override
 	public UserDto createUser(UserDto user) {
-		return null;
+		
+		UserEntity userEntity = new UserEntity();
+		BeanUtils.copyProperties(user, userEntity);
+		
+		UserEntity storedUserDetails = userRepository.save(userEntity);
+		
+		UserDto returnValue = new UserDto();
+		BeanUtils.copyProperties(storedUserDetails, returnValue);
+		
+		return returnValue;
 	}
 }
